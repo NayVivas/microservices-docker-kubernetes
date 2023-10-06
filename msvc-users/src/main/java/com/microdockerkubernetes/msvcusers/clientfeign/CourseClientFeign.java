1b4201ca32e0f4b72e9b66729fb21d34cefd71ac
NayVivas <casa24nov@gmail.com>
2023-10-06 18:30:01 -0300

    docker

--- msvc-users/src/main/java/com/microdockerkubernetes/msvcusers/clientfeign/CourseClientFeign.java
+++ msvc-users/src/main/java/com/microdockerkubernetes/msvcusers/clientfeign/CourseClientFeign.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="msvc-courses", url = "http://localhost:8002/api/course")
+@FeignClient(name="msvc-courses", url = "host.docker.internal:8002/api/course")
 public interface CourseClientFeign {
     @DeleteMapping("/delete-user/{id}")
     void deleteCourseUserId(@PathVariable Long id);
