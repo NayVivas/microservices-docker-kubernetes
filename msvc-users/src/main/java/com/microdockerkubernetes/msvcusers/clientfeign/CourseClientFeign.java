affe71a724b5689c6aed1867d6a64878537195e7
NayVivas <casa24nov@gmail.com>
2023-10-10 14:52:13 -0300

    Dockerfile

--- msvc-users/src/main/java/com/microdockerkubernetes/msvcusers/clientfeign/CourseClientFeign.java
+++ msvc-users/src/main/java/com/microdockerkubernetes/msvcusers/clientfeign/CourseClientFeign.java
@@ -1,11 +1,10 @@
 package com.microdockerkubernetes.msvcusers.clientfeign;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="msvc-courses", url = "host.docker.internal:8002/api/course")
+@FeignClient(name="msvc-courses", url = "http://msvc-courses:8002/api/course")
 public interface CourseClientFeign {
     @DeleteMapping("/delete-user/{id}")
     void deleteCourseUserId(@PathVariable Long id);
